ef3a147f1727d080175af9b8fad4523304488cfa
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-27 00:52:00 +0200

    Setup a simple login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'books#index'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  post 'book', to: 'books#create'
+  get 'users/:username', to: 'users#show', as: 'profile'
 end
