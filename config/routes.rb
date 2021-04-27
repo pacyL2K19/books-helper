27f6fc3aee1939175ba81fa686d9215729635a03
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-27 22:32:47 +0200

    Minor update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'books#index'
+  root 'books#new'
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
