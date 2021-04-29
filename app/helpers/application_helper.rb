43f83b2eadf658344fae5fad1953bf871b089844
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-29 03:29:44 +0200

    FIx the avatar issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
 
   def profile_pic(usr, size = '')
     if usr.photo.present?
-      image_tag(usr.photo.to_s, alt: usr.username, class: "pr-2 rounded-circle profile-img #{size}")
+      image_tag(usr.photo.to_s, alt: usr.username, class: "rounded mx-2 profile-img #{size}")
     else
       image_tag('https://source.unsplash.com/random/60x60',
                 alt: usr.username, class: 'pr-2 rounded-circle profile-img')
