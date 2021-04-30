bb9393107a7833157eef632c334b4076ecda578b
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-30 20:59:56 +0200

    Add negative test to spec files

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Following, type: :model do
     it { should belong_to(:follower) }
     it { should belong_to(:followed) }
   end
+
+  describe 'Attributes Validation' do
+    user_one = User.create(username: 'Paci',
+                           fullname: 'Plic')
+    following = user_one.followings.new(followed_id: nil)
+
+    it 'should invalidate the followed_id of nil as value' do
+      expect(following).to_not be_valid
+    end
+  end
 end
--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Opinion, type: :model do
   describe 'opinion Association' do
     it { should belong_to(:author) }
   end
+  describe 'Attributes Validation' do
+    # user_one = User.create(username: 'Paci',
+    #                        fullname: 'Plic')
+    opinion = Opinion.new(text: nil)
+
+    it 'should invalidate opinion with nil as value' do
+      expect(opinion).to_not be_valid
+    end
+  end
 end
